192494dfd59c856cbf7b10a9661aed4d9ce62ca1
frte--free <demasamson05@gmail.com>
2024-05-13 12:37:12 +0300

    r1

---
+++ C.java
@@ -0,0 +1,38 @@
+public class C implements K, B {
+
+    private String b = "hello";
+
+    private int h = 42;
+
+    public double ad() {
+        return 9.11;
+    }
+
+    public byte oo() {
+        return 3;
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public String kk() {
+        return "Hello world";
+    }
+
+    public double ee() {
+        return 500.100;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public Object rr() {
+        return null;
+    }
+}
