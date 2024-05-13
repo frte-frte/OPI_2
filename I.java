9564a3821164e82a870dd6de6a9850d715bb189c
frte--free <demasamson05@gmail.com>
2024-05-13 12:37:31 +0300

    r2

---
+++ I.java
@@ -0,0 +1,30 @@
+public class I implements K {
+
+    private double g = 100.500;
+
+    private String j = "init";
+
+    public int hh() {
+        return new java.util.Random(10).nextInt(10);
+    }
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public String kk() {
+        return "No";
+    }
+
+    public int[] ii() {
+        return new int[]{4, 3, 2, 1};
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+}
